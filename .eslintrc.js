5c078340fab747f3d17f05dc94f32c43b169bec9
Alissa Huskey <alissa.huskey@gmail.com>
2025-05-14 04:44:49 -0600

    Add es2021: true to the config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
   extends: [
     "plugin:jsdoc/recommended",
   ],
+  // the error will occurr if either or both of the following are present
+  env: {
+    es2021: true
+  },
   parserOptions: {
     ecmaVersion: "latest",
   },
